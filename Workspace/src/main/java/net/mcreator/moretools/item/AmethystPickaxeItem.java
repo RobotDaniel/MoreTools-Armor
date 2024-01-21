e0fead367749688d441405779a2fd61e0ba6a543
RobotDaniel <101746899+RobotDaniel@users.noreply.github.com>
2024-01-21 11:31:50 +0000

    added prelese 1.5 for 1.19.4

--- Workspace/src/main/java/net/mcreator/moretools/item/AmethystPickaxeItem.java
+++ Workspace/src/main/java/net/mcreator/moretools/item/AmethystPickaxeItem.java
@@ -1,6 +1,8 @@
 
 package net.mcreator.moretools.item;
 
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.PickaxeItem;
@@ -8,6 +10,10 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.moretools.procedures.AmethystPickaxeBlockDestroyedWithToolProcedure;
 
 public class AmethystPickaxeItem extends PickaxeItem {
 	public AmethystPickaxeItem() {
@@ -36,5 +42,12 @@ public class AmethystPickaxeItem extends PickaxeItem {
 				return Ingredient.of(new ItemStack(Items.AMETHYST_SHARD));
 			}
 		}, 1, -3f, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS));
+	}
+
+	@Override
+	public boolean mineBlock(ItemStack itemstack, Level world, BlockState blockstate, BlockPos pos, LivingEntity entity) {
+		boolean retval = super.mineBlock(itemstack, world, blockstate, pos, entity);
+		AmethystPickaxeBlockDestroyedWithToolProcedure.execute(entity);
+		return retval;
 	}
 }
